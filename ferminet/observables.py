985a82683d780633df10ba01aa10e49718dfc73a
Konstantin Tokarev <annulen@yandex.ru>
2025-09-03 15:12:52 +0300

    Add get_rho_He_2

--- ferminet/observables.py
+++ ferminet/observables.py
@@ -451,7 +451,7 @@ def make_spin_rho(
       data: networks.FermiNetData,
       state: None = None,
   ) -> jnp.ndarray:
-    return density.get_rho_2(
+    return density.get_rho_He_2(
         batch_signed_net,
         params,
         cfg.system.ndim,
