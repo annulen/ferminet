3435004379d2c6d0aa06a776028178af7be3b2ed
Konstantin Tokarev <annulen@yandex.ru>
2025-09-23 17:01:27 +0300

    Write all returned values to csv

--- ferminet/train.py
+++ ferminet/train.py
@@ -1061,8 +1061,8 @@ def train(cfg: ml_collections.ConfigDict, writer_manager=None):
             logging_str += ', <W0>=' + '%03.4f,' * 10,
             logging_args += *obs_data,
           elif key == 'spin_rho':
-            writer_kwargs[key] = obs_data
             obs_data_list = obs_data.reshape(-1)
+            writer_kwargs[key] = ",".join(map(lambda x: f"{x}", obs_data_list))
             logging_str += ', <S_rho>=' + '%03.4f,' * obs_data_list.shape[0]
             logging_args += *obs_data_list,
 
